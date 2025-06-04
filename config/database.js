5c7fa18233f1376eac0a1c82c1fd372e6615e019
ShreyasVH <shreyas.deve.1@gmail.com>
2025-06-04 10:45:12 +0530

    added cli scripts

--- config/database.js
+++ config/database.js
@@ -6,7 +6,6 @@ async function connectDatabase() {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log('Connected to MongoDB');
   } catch (error) {
     console.error('Failed to connect to MongoDB:', error);
     throw error;
