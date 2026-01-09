4f70e19de667b4c9a9c2b5b45172d1ef227fe254
ShreyasVH <shreyas.deve.1@gmail.com>
2026-01-09 08:29:19 +0530

    using mongo 8.2.3

--- config/database.js
+++ config/database.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose');
 async function connectDatabase() {
   try {
     await mongoose.connect('mongodb://' + process.env.MONGODB_IP + ':' + process.env.MONGODB_PORT + '/' + process.env.MONGODB_DB, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
     });
   } catch (error) {
     console.error('Failed to connect to MongoDB:', error);
